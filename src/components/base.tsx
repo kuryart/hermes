6db4f58cbac1c95f00db881525d6d0f4b7d5d0ae
kuryart <kuryart1@gmail.com>
2023-11-09 20:11:46 -0300

    add manifest and icons to base layout

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -11,6 +11,11 @@ export const BaseHtml = ({ children }: PropsWithChildren) => (
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>THE BETH STACK</title>
+      <link rel="icon" type="image/x-icon" href="/public/icons/hermes.png" />
+
+      <link rel="manifest" href="/public/pwa.webmanifest" />
+      <link rel="stylesheet" href="/public/dist/unocss.css" />
+
       <script src="https://unpkg.com/htmx.org@1.9.5"></script>
       <script src="https://unpkg.com/htmx.org/dist/ext/response-targets.js"></script>
       <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
@@ -18,7 +23,7 @@ export const BaseHtml = ({ children }: PropsWithChildren) => (
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@unocss/reset/tailwind.min.css"
       />
-      <link rel="stylesheet" href="/public/dist/unocss.css" />
+
       <script>{safeScript}</script>
     </head>
     <body hx-boost="true" class="h-screen">
@@ -26,6 +31,15 @@ export const BaseHtml = ({ children }: PropsWithChildren) => (
         Create BETH App
       </h1>
       {children}
+
+      <footer>
+        <a
+          href="https://www.flaticon.com/br/icones-gratis/hermes"
+          title="hermes ícones"
+        >
+          Hermes ícones criados por Freepik - Flaticon
+        </a>
+      </footer>
     </body>
   </html>
 );
